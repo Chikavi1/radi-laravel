31a790981e624382f87af6b85b2ce74297a13eca
Chikavi1 <chikavi@hotmail.com>
2023-06-12 20:53:45 -0500

    download redirect and placas

--- resources/views/home/download.blade.php
+++ resources/views/home/download.blade.php
@@ -36,5 +36,32 @@
   </div>
 </div>
 
+<script>
+function getMobileOperatingSystem() {
+    var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+
+    // Windows Phone must come first because its UA also contains "Android"
+    if (/windows phone/i.test(userAgent)) {
+        return "Windows Phone";
+    }
+
+    if (/android/i.test(userAgent)) {
+        // return "Android";
+        window.location.href = "https://play.google.com/store/apps/details?id=pet.radi.radi";
+
+    }
+
+    // iOS detection from: http://stackoverflow.com/a/9039885/177710
+    if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+        // return "iOS";
+        window.location.href = "https://apps.apple.com/us/app/radi-pets/id1637462305";
+
+    }
+
+    return "unknown";
+}
+
+getMobileOperatingSystem()
+</script>
 
 @include('layouts.footer')
--- resources/views/home/placas.blade.php
+++ resources/views/home/placas.blade.php
@@ -6,51 +6,18 @@
       Guarda la información de tu <span class="block w-full text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-purple-500 lg:inline">mascota</span> en un solo lugar
     </h1>
 
-
+    <div class="text-center">
+        <p class="text-sm" >Precio</p>
+        <h2 class="text-4xl font-bold text-green-800">$389 MXN</h2>
+        <p class="mb-8">Incluye envio en México</p>
+        <a target="_blank" href="https://buy.stripe.com/5kAaH4amiaSa6SAdQR" class="w-full shadow-xl btn btn-primary bg-purple-800 px-8 py-4 text-white rounded-md btn-lg sm:w-auto">Comprar</a>
+      </div>
     <div class="mb-4 space-x-0 md:space-x-2 md:mb-8">
       <!-- <a class="bg-purple-800 p-4 text-white inline-flex items-center justify-center w-full mb-2 btn btn-primary btn-lg sm:w-auto sm:mb-0" href="#">
         Comprar
       </a> -->
     </div>
 
-    <div class="mx-auto ml-0 md:ml-58 lg:ml-72 get-app flex space-x-5 mt-10 justify-center md:justify-start">
-      <a href="https://apps.apple.com/us/app/radi/id1637462305">
-          <div class="apple bg-white shadow-md px-3 py-2 rounded-lg flex items-center space-x-4">
-              <div class="logo">
-                  <svg
-                      class="w-6 h-6"
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="419.955" height="512"
-                      viewBox="0 0 419.955 512">
-                      <g transform="translate(-46.022)">
-                          <path d="M185.255,512c-76.2-.439-139.233-155.991-139.233-235.21,0-129.4,97.075-157.734,134.487-157.734,16.86,0,34.863,6.621,50.742,12.48,11.1,4.087,22.588,8.306,28.975,8.306,3.823,0,12.832-3.589,20.786-6.738,16.963-6.753,38.071-15.146,62.651-15.146h.146c18.354,0,74,4.028,107.461,54.272l7.837,11.777-11.279,8.511c-16.113,12.158-45.513,34.336-45.513,78.267,0,52.031,33.3,72.041,49.292,81.665,7.061,4.248,14.37,8.628,14.37,18.208,0,6.255-49.922,140.566-122.417,140.566-17.739,0-30.278-5.332-41.338-10.034-11.191-4.761-20.845-8.862-36.8-8.862-8.086,0-18.311,3.823-29.136,7.881C221.5,505.73,204.752,512,185.753,512Z"/><path d="M351.343,0c1.888,68.076-46.8,115.3-95.425,112.342C247.9,58.015,304.54,0,351.343,0Z"/>
-                      </g>
-                  </svg>
-              </div>
-              <div class="text">
-                  <p class=" text-xs text-gray-600">Descargar en</p>
-                  <p class=" text-xs font-semibold text-gray-900">Apple Store</p>
-              </div>
-          </div>
-      </a>
-      <a href='https://play.google.com/store/apps/details?id=pet.radi.radi' target="_blank" class="google bg-white shadow-md px-3 py-2 rounded-lg flex items-center space-x-4">
-          <div class="image">
-              <svg
-                  class="w-6 h-6"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="436.057" height="469.847"
-                  viewBox="0 0 436.057 469.847">
-                  <g transform="translate(-16.896)">
-                      <path d="M270.336,234.965,34.39,462.165A40.146,40.146,0,0,1,16.9,428.672V41.258A40.146,40.146,0,0,1,34.39,7.765Z" fill="#2196f3"/><path d="M352.9,155.6l-82.56,79.36L34.39,7.765a31.446,31.446,0,0,1,2.773-1.92A40.363,40.363,0,0,1,77.91,5.2Z" fill="#4caf50"/><path d="M452.95,234.965a40.791,40.791,0,0,1-21.333,36.267L352.9,314.325l-82.56-79.36L352.9,155.6l78.72,43.093A40.791,40.791,0,0,1,452.95,234.965Z" fill="#ffc107"/><path d="M352.9,314.325,77.91,464.725a40.9,40.9,0,0,1-40.747-.64,31.44,31.44,0,0,1-2.773-1.92l235.947-227.2Z" fill="#f44336"/>
-                  </g>
-              </svg>
-          </div>
-          <div class="text">
-              <p class="text-xs text-gray-600" >Descarga Radi</p>
-              <p class="text-xs font-semibold text-gray-900">Google play</p>
-          </div>
-      </a>
-  </div>
   </div>
   <div class="w-full mx-auto mt-20 text-center md:w-10/12">
     <div class="grid grid-cols-12 gap-2">
@@ -67,86 +34,80 @@
   </div>
 </section>
 
-<section class="px-4 py-24 mx-auto max-w-7xl md:px-32">
-  <div class="grid items-center w-full grid-cols-1 gap-0 mx-auto lg:grid-cols-11 lg:gap-24 xl:w-11/12">
-    <div class="col-auto text-center md:col-span-5 lg:text-left">
-      <h2 class="mb-3 text-3xl font-extrabold leading-tight tracking-tight text-center text-black sm:text-left md:text-4xl">¿Cómo funciona?</h2>
-      <p class="mb-6 text-lg text-left text-gray-600 md:text-xl">Ingresa a nuestra app para registrar la información de tu mascota. <br> Después de enlazar la placa con el perfil de tu mascota, toda la información será mostrada a las personas que escanee la placa con el código QR. <br> La información podrás modificarla cuando tú desees. </p>
-
-    </div>
-    <div class="col-auto md:col-span-6">
-            <img src="https://radi-images.s3.us-west-1.amazonaws.com/radi-perfil.png" class="p-4" alt="">
-      <!-- <img src="https://radi-images.s3.us-west-1.amazonaws.com/tag-radi.png" class="p-4" alt=""> -->
-    </div>
-  </div>
-</section>
-
-
 <section class="px-4 py-20 mx-auto max-w-7xl lg:px-12" >
-  <div class="grid items-center grid-cols-1 lg:grid-cols-2 gap-y-10 lg:gap-y-32 gap-x-10 lg:gap-x-24">
-    <div>
-      <h2 class="mb-3 text-3xl font-extrabold leading-tight tracking-tight text-center text-black sm:text-left md:text-4xl">Beneficios de la placa</h2>
-      <p class="mb-6 text-lg text-center text-gray-600 sm:text-left md:text-xl">Conoce los beneficios de tener una placa de Radi Pets para sacarle el mayor provecho a la placa.</p>
-      <div class="flex items-start">
-        <svg viewBox="0 0 20 20" fill="currentColor" class="flex-none w-5 h-5 mt-1 mr-2 text-primary">
-          <path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path>
-        </svg>
-        <p class="text-lg text-gray-700"><b>Identificación rápida y fácil:</b>  Si tu mascota se pierde, una placa de identificación con QR y NFC permite a cualquier persona que la encuentre identificar rápidamente a su mascota y ponerse en contacto con usted para devolverla a casa.</p>
+    <div class="grid items-center grid-cols-1 lg:grid-cols-2 gap-y-10 lg:gap-y-32 gap-x-10 lg:gap-x-24">
+      <div>
+        <h2 class="mb-3 text-3xl font-extrabold leading-tight tracking-tight text-center text-black sm:text-left md:text-4xl">Beneficios de la placa</h2>
+        <p class="mb-6 text-lg text-center text-gray-600 sm:text-left md:text-xl">Conoce los beneficios de tener una placa de Radi Pets para sacarle el mayor provecho a la placa.</p>
+        <div class="flex items-start">
+          <svg viewBox="0 0 20 20" fill="currentColor" class="flex-none w-5 h-5 mt-1 mr-2 text-primary">
+            <path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path>
+          </svg>
+          <p class="text-lg text-gray-700"><b>Identificación rápida y fácil:</b>  Si tu mascota se pierde, una placa de identificación con QR y NFC permite a cualquier persona que la encuentre identificar rápidamente a su mascota y ponerse en contacto con usted para devolverla a casa.</p>
+        </div>
+        <div class="flex items-start mt-4">
+          <svg viewBox="0 0 20 20" fill="currentColor" class="flex-none w-5 h-5 mt-1 mr-2 text-primary">
+            <path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path>
+          </svg>
+          <p class="text-lg text-gray-700"><b>Recibe notificaciones:</b>  Si tu mascota se llegara a perder, cuando alguien la escanee te llegara una notificación a tu teléfono.</p>
+        </div>
+        <div class="flex items-start mt-4">
+          <svg viewBox="0 0 20 20" fill="currentColor" class="flex-none w-5 h-5 mt-1 mr-2 text-primary">
+            <path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path>
+          </svg>
+          <p class="text-lg text-gray-700"><b>Fácil de actualizar:</b> Si necesita actualizar cualquier información en la placa de identificación, puede hacerlo fácilmente sin tener que reemplazar la placa en sí. Puede simplemente actualizar la información en línea y la información se actualizará automáticamente en la placa.</p>
+        </div>
       </div>
-      <div class="flex items-start mt-4">
-        <svg viewBox="0 0 20 20" fill="currentColor" class="flex-none w-5 h-5 mt-1 mr-2 text-primary">
-          <path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path>
-        </svg>
-        <p class="text-lg text-gray-700"><b>Recibe notificaciones:</b>  Si tu mascota se llegara a perder, cuando alguien la escanee te llegara una notificación a tu teléfono.</p>
+      <div class="flex flex-col flex-grow space-y-5">
+        <div class="flex items-start">
+          <svg viewBox="0 0 20 20" fill="currentColor" class="flex-none w-5 h-5 mt-1 mr-2 text-primary">
+            <path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path>
+          </svg>
+          <p class="text-lg text-gray-700"><b> Acceso a información importante:</b> Puede ingresar información sobre su mascota, como su nombre, dirección,información médica y cualquier otro dato que desee compartir.</p>
+        </div>
+
+        <!-- <div class="flex items-start">
+          <svg viewBox="0 0 20 20" fill="currentColor" class="flex-none w-5 h-5 mt-1 mr-2 text-primary">
+            <path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path>
+          </svg>
+          <p class="text-lg text-gray-700">Descuentos en establecimientos.</p>
+        </div> -->
+
+
+
+        <div class="flex items-start">
+          <svg viewBox="0 0 20 20" fill="currentColor" class="flex-none w-5 h-5 mt-1 mr-2 text-primary">
+            <path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path>
+          </svg>
+          <p class="text-lg text-gray-700"><b>Durabilidad:</b>  Las placas de identificación con QR y NFC están diseñadas para ser duraderas y resistentes a los elementos y a la actividad diaria de su mascota. </p>
+        </div>
+        <div class="flex items-start">
+          <svg viewBox="0 0 20 20" fill="currentColor" class="flex-none w-5 h-5 mt-1 mr-2 text-primary">
+            <path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path>
+          </svg>
+          <p class="text-lg text-gray-700"><b>Ahorra tiempo:</b> Al proporcionar una forma fácil y rápida de identificar a su mascota, una placa de identificación con QR y NFC puede ahorrarle tiempo, dinero y preocupaciones al buscar a su mascota perdida.
       </div>
-      <div class="flex items-start mt-4">
-        <svg viewBox="0 0 20 20" fill="currentColor" class="flex-none w-5 h-5 mt-1 mr-2 text-primary">
-          <path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path>
-        </svg>
-        <p class="text-lg text-gray-700"><b>Fácil de actualizar:</b> Si necesita actualizar cualquier información en la placa de identificación, puede hacerlo fácilmente sin tener que reemplazar la placa en sí. Puede simplemente actualizar la información en línea y la información se actualizará automáticamente en la placa.</p>
+
       </div>
     </div>
-    <div class="flex flex-col flex-grow space-y-5">
-      <div class="flex items-start">
-        <svg viewBox="0 0 20 20" fill="currentColor" class="flex-none w-5 h-5 mt-1 mr-2 text-primary">
-          <path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path>
-        </svg>
-        <p class="text-lg text-gray-700"><b> Acceso a información importante:</b> Puede ingresar información sobre su mascota, como su nombre, dirección,información médica y cualquier otro dato que desee compartir.</p>
-      </div>
-
-      <!-- <div class="flex items-start">
-        <svg viewBox="0 0 20 20" fill="currentColor" class="flex-none w-5 h-5 mt-1 mr-2 text-primary">
-          <path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path>
-        </svg>
-        <p class="text-lg text-gray-700">Descuentos en establecimientos.</p>
-      </div> -->
-
-
-
-      <div class="flex items-start">
-        <svg viewBox="0 0 20 20" fill="currentColor" class="flex-none w-5 h-5 mt-1 mr-2 text-primary">
-          <path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path>
-        </svg>
-        <p class="text-lg text-gray-700"><b>Durabilidad:</b>  Las placas de identificación con QR y NFC están diseñadas para ser duraderas y resistentes a los elementos y a la actividad diaria de su mascota. </p>
-      </div>
-      <div class="flex items-start">
-        <svg viewBox="0 0 20 20" fill="currentColor" class="flex-none w-5 h-5 mt-1 mr-2 text-primary">
-          <path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path>
-        </svg>
-        <p class="text-lg text-gray-700"><b>Ahorra tiempo:</b> Al proporcionar una forma fácil y rápida de identificar a su mascota, una placa de identificación con QR y NFC puede ahorrarle tiempo, dinero y preocupaciones al buscar a su mascota perdida.
-    </div>
-
-    </div>
-  </div>
 </section>
 
-<div class="text-center">
-  <p class="text-2xl" >Precio</p>
-  <h2 class="text-4xl font-bold text-green-800">$389 MXN</h2>
-  <p class="mb-8">Incluye envio en México</p>
-  <a target="_blank" href="https://buy.stripe.com/5kAaH4amiaSa6SAdQR" class="w-full shadow-xl btn btn-primary bg-purple-800 px-8 py-4 text-white rounded-md btn-lg sm:w-auto">Comprar</a>
+<section class="px-4 py-24 mx-auto max-w-7xl md:px-32">
+    <div class="grid items-center w-full grid-cols-1 gap-0 mx-auto lg:grid-cols-11 lg:gap-24 xl:w-11/12">
 
-</div>
+      <div class="col-auto text-center md:col-span-5 lg:text-left">
+        <h2 class="mb-3 text-3xl font-extrabold leading-tight tracking-tight text-center text-black sm:text-left md:text-4xl">¿Cómo funciona?</h2>
+        <p class="mb-6 text-lg text-left text-gray-600 md:text-xl">Ingresa a nuestra app para registrar la información de tu mascota. <br> Después de enlazar la placa con el perfil de tu mascota, toda la información será mostrada a las personas que escanee la placa con el código QR. <br> La información podrás modificarla cuando tú desees. </p>
+
+      </div>
+
+      <div class="col-auto md:col-span-6">
+              <img src="https://radi-images.s3.us-west-1.amazonaws.com/radi-perfil.png" class="p-4" alt="">
+        <!-- <img src="https://radi-images.s3.us-west-1.amazonaws.com/tag-radi.png" class="p-4" alt=""> -->
+      </div>
+    </div>
+  </section>
+
 
 
 <div class="m-4 container">
@@ -155,8 +116,8 @@
       <p class="mb-2 text-xs font-semibold tracking-wide text-gray-400 uppercase">Fácil y Rápido, Menos de 4 minutos</p>
       <h1 class="mb-3 text-3xl font-bold leading-tight text-gray-900 md:text-4xl">Descarga la app</h1>
       <p class="mb-5 text-base text-gray-500 md:text-lg">
-        Con nuestra app, podrás mantener el historial clínico y la información de tu mascota en un solo lado, siempre seguro. Además, podrás guardar tus paseos y conocer lugares pet friendly cerca de ti y planificar paseos divertidos y seguros para tu mascota.
-      <br>  ¡Descarga la app hoy y haz que tus paseos con mascotas sean una aventura!
+        Con nuestra app, podrás mantener el historial clínico y la información de tu mascota en un solo lado, siempre seguro. Además, conocer lugares pet friendly cerca de ti y planificar paseos divertidos y seguros para tu mascota.
+      <br>  ¡Descarga la app hoy y haz que tus salidas con tus mascotas sean una aventura!
         <!-- <p>Proximamente</p> -->
       <div class="get-app flex space-x-5 mt-10 justify-center md:justify-start">
         <a href="https://apps.apple.com/us/app/radi/id1637462305">
