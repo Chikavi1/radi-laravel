bc6d80eaea920d5588794fd1ff64db64c712cae8
Chikavi1 <chikavi@hotmail.com>
2023-06-21 15:11:58 -0500

    home controller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -19,8 +19,8 @@ class HomeController extends Controller
     {
         SEO::setTitle('La plataforma para los amantes de las mascotas');
         SEO::setDescription('Encuentra lugares pet friendly, adopta a una mascota o apoya a los albergues, todo en un solo lugar.');
-        SEO::opengraph()->setUrl('https://radi.pet');
-        SEO::setCanonical('https://radi.pet');
+        SEO::opengraph()->setUrl('https://radi.pet/');
+        SEO::setCanonical('https://radi.pet/');
         SEO::opengraph()->addProperty('type', 'articles');
         SEO::opengraph()->addImage(asset('img/default.png'));
         SEO::twitter()->setImage(asset('img/default.png'));
@@ -36,8 +36,8 @@ class HomeController extends Controller
     {
         SEO::setTitle('Contacta a nuestro equipo');
         SEO::setDescription('Contacta a nuestro equipo de expertos para obtener ayuda y asesoramiento en cualquier tema relacionado con nuestros servicios. Estamos disponibles para responder a tus preguntas y ayudarte en lo que necesites. ¡Contáctanos hoy mismo y descubre cómo podemos ayudarte a alcanzar tus objetivos!');
-        SEO::opengraph()->setUrl('https://radi.pet');
-        SEO::setCanonical('https://radi.pet');
+        SEO::opengraph()->setUrl('https://radi.pet/contact');
+        SEO::setCanonical('https://radi.pet/contact');
         SEO::opengraph()->addProperty('type', 'articles');
         SEO::opengraph()->addImage(asset('img/default.png'));
         SEO::twitter()->setImage(asset('img/default.png'));
@@ -48,8 +48,8 @@ class HomeController extends Controller
     {
         SEO::setTitle('Términos y condiciones');
         SEO::setDescription('Obtén información importante sobre tus derechos y responsabilidades al utilizar nuestros productos y servicios');
-        SEO::opengraph()->setUrl('https://radi.pet');
-        SEO::setCanonical('https://radi.pet');
+        SEO::opengraph()->setUrl('https://radi.pet/terms');
+        SEO::setCanonical('https://radi.pet/terms');
         SEO::opengraph()->addProperty('type', 'articles');
         SEO::opengraph()->addImage(asset('img/default.png'));
         SEO::twitter()->setImage(asset('img/default.png'));
@@ -59,8 +59,8 @@ class HomeController extends Controller
     {
         SEO::setTitle('Política de privacidad');
         SEO::setDescription('Lee nuestras políticas de privacidad para entender cómo manejamos la información personal que recopilamos de nuestros usuarios. Protegemos tus datos personales y nos comprometemos a mantener la confidencialidad de la información que compartes con nosotros.');
-        SEO::opengraph()->setUrl('https://radi.pet');
-        SEO::setCanonical('https://radi.pet');
+        SEO::opengraph()->setUrl('https://radi.pet/privacy');
+        SEO::setCanonical('https://radi.pet/privacy');
         SEO::opengraph()->addProperty('type', 'articles');
         SEO::opengraph()->addImage(asset('img/default.png'));
         SEO::twitter()->setImage(asset('img/default.png'));
@@ -130,8 +130,8 @@ class HomeController extends Controller
     {
         SEO::setTitle('Verificación de adopción');
         SEO::setDescription('Verifica si la adopción fue en Radi Pets.');
-        SEO::opengraph()->setUrl('https://radi.pet/placas');
-        SEO::setCanonical('https://radi.pet/placas');
+        SEO::opengraph()->setUrl('https://radi.pet/validate_pet');
+        SEO::setCanonical('https://radi.pet/validate_pet');
         SEO::opengraph()->addProperty('type', 'articles');
         SEO::opengraph()->addImage(asset('img/default.png'));
         SEO::twitter()->setImage(asset('img/default.png'));
