b34d9a24267710ec4a89844788019cc675a8648a
Chikavi1 <chikavi@hotmail.com>
2023-07-01 00:49:00 -0500

    patadas de ahogado 3

--- resources/views/lost/show.blade.php
+++ resources/views/lost/show.blade.php
@@ -7,14 +7,14 @@
 <meta property="og:url" content="https://radi.pet/lost/{{$lost->setHiddenId()}}" />
 <meta property="og:title" content="Ayúdanos a que {{$lost->pet->name}} regrese a casa - Radi Pets" />
 <meta property="og:description" content="{{$lost->note}}" />
-<meta property="og:image" content="$lost->pet->photo" />
+<meta property="og:image" content="{{$lost->pet->photo}}" />
 
 <!-- Twitter -->
 <meta property="twitter:card" content="summary_large_image" />
 <meta property="twitter:url" content="https://radi.pet/lost/{{$lost->setHiddenId()}}" />
 <meta property="twitter:title" content="Ayúdanos a que {{$lost->pet->name}} regrese a casa - Radi Pets" />
 <meta property="twitter:description" content="{{$lost->note}}" />
-<meta property="twitter:image" content="$lost->pet->photo" />
+<meta property="twitter:image" content="{{$lost->pet->photo}}" />
 
 <!-- Meta Tags Generated with https://metatags.io -->
 
