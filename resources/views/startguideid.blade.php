f7c22e1347608efd2d96d4322c13443fb262899c
Chikavi1 <chikavi@hotmail.com>
2023-10-13 00:15:26 -0600

    renew

--- resources/views/startguideid.blade.php
+++ resources/views/startguideid.blade.php
@@ -1,62 +1,120 @@
-<script src="https://cdn.jsdelivr.net/gh/aframevr/aframe@1c2407b26c61958baa93967b5412487cd94b290b/dist/aframe-master.min.js"></script>
-<script src="https://raw.githack.com/AR-js-org/AR.js/master/aframe/build/aframe-ar-nft.js"></script>
+<link
+href="https://cdn.jsdelivr.net/npm/tailwindcss/dist/tailwind.min.css"
+rel="stylesheet"
+/>
+<meta name="viewport" content="width=device-width, initial-scale=1">
 
-<style>
-  .arjs-loader {
-    height: 100%;
-    width: 100%;
-    position: absolute;
-    top: 0;
-    left: 0;
-    background-color: rgba(0, 0, 0, 0.8);
-    z-index: 9999;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
+<link
+rel="stylesheet"
+href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
+/>
 
-  .arjs-loader div {
-    text-align: center;
-    font-size: 1.25em;
-    color: white;
-  }
-</style>
+<section class="px-4 py-24 mx-auto max-w-7xl">
+    <div class="w-full mx-auto text-left md:w-11/12 xl:w-9/12 md:text-center">
+      <h1 class="mt-12 md:mt-2 animate__animated animate__slideInDown mb-6 text-4xl font-semibold leading-none tracking-normal text-gray-900 md:text-6xl md:tracking-tight">
+        Bienvenido a la nueva experiencia de las Placas de identificacion      </h1>
 
-<body style="margin : 0px; overflow: hidden;">
-  <!-- minimal loader shown until image descriptors are loaded -->
-  <div class="arjs-loader">
-    <div>Loading, please wait...</div>
-  </div>
-  <a-scene
-    vr-mode-ui="enabled: false;"
-    renderer="logarithmicDepthBuffer: true;"
-    embedded
-    arjs="trackingMethod: best; sourceType: webcam;debugUIEnabled: false;"
-  >
-    <!-- we use cors proxy to avoid cross-origin problems -->
+      <div class="mb-4 space-x-0 md:space-x-2 md:mb-8">
+      </div>
+    </div>
+    <div class="w-full mx-auto mt-12 text-center ">
+      <img src="https://i.ibb.co/jDGpDYN/1.png" alt="id radi" class="animate__animated animate__rollIn w-72 mx-auto" />
+    </div>
+  </section>
 
-    <!--
-      ⚠️⚠️⚠️
-      https://arjs-cors-proxy.herokuapp.com/ is now offline, Heroku has dismissed all his free plans from November 2022.
-      You need to host your own proxy and use it instead. The proxy is based on CORS Anywhere (see https://github.com/Rob--W/cors-anywhere).
-      ⚠️⚠️⚠️
-    -->
+  <div class="grid grid-cols-12 gap-4 p-6 mb-12">
+    <div class="col-span-12 md:col-span-4">
+            <div class="hover:-translate-y-1 hover:scale-150 duration-500 relative w-full p-4 overflow-hidden bg-black shadow-lg rounded-2xl">
+                <img alt="moto" src="https://i.ibb.co/XxVtwBL/Post-facebook-venta-de-producto-comercial-moderno-gris-2.png" class="rounded-xl w-full  mb-4 -right-20 -bottom-8"/>
+                <div class="w-4/6">
+                    <p class="mb-2 text-lg font-medium text-white">
+                        Garantia de Radi Pets
+                    </p>
+                    <p class="text-xs text-gray-400">
+                        En Radi Pets, respaldamos nuestros productos con una garantía sólida de 1 mes. Estamos comprometidos a brindar el mejor servicio a nuestros clientes.
+                </div>
+            </div>
 
-    <a-nft
-      type="nft"
-      url="https://arjs-cors-proxy.herokuapp.com/https://raw.githack.com/AR-js-org/AR.js/master/aframe/examples/image-tracking/nft/trex/trex-image/trex"
-      smooth="true"
-      smoothCount="10"
-      smoothTolerance=".01"
-      smoothThreshold="5"
-    >
-      <a-entity
-        gltf-model="https://arjs-cors-proxy.herokuapp.com/https://raw.githack.com/AR-js-org/AR.js/master/aframe/examples/image-tracking/nft/trex/scene.gltf"
-        scale="5 5 5"
-        position="50 150 0"
-      >
-      </a-entity>
-    </a-nft>
-    <a-entity camera></a-entity>
-  </a-scene>
-</body>
+    </div>
+    <div class="col-span-12 md:col-span-4">
+            <div class="hover:-translate-y-1 hover:scale-150  duration-500 relative w-full p-4 overflow-hidden bg-white shadow-lg rounded-2xl">
+                <img alt="moto" src="https://i.ibb.co/qD4RCZR/Post-facebook-venta-de-producto-comercial-moderno-gris-1.png" class="rounded-xl w-full object-cover  mb-4 -right-20 -bottom-8"/>
+                <div class="w-full">
+                    <p class="mb-2 text-lg font-medium text-gray-800">
+                       Seguridad en tu información
+                    </p>
+                    <p class="text-xs w-full text-gray-700">
+                        Priorizamos la privacidad de nuestros usuarios. Seguimos altos estándares de seguridad de datos y nunca compartimos tu información de contacto la cual solo se comparte cuando alguien escanea el QR de tu mascota desaparecida.                    </p>
+
+                </div>
+            </div>
+
+    </div>
+    <div class="col-span-12 md:col-span-4">
+            <div style="background:#0348ac" class="hover:-translate-y-1 hover:scale-150   duration-500 relative w-full p-4 overflow-hiddeshadow-lg rounded-2xl">
+                <img alt="moto" src="https://i.ibb.co/GkS0mhF/Post-facebook-venta-de-producto-comercial-moderno-gris-3.png" class="rounded-xl   h-42 mb-4 -right-20 -bottom-8"/>
+                <div class="w-4/6">
+                    <p class="mb-2 text-lg font-medium text-white">
+                        Diseño Único e Incomparable
+                    </p>
+                    <p class="text-xs text-gray-400">
+                        Las placas de Radi Pets destacan por su diseño único, marcando la diferencia frente a las placas tradicionales y ofreciendo beneficios exclusivos en el mercado.
+                    </p>
+                </div>
+            </div>
+    </div>
+</div>
+
+<h2 class="text-center text-2xl md:text-5xl my-12 md:my-20">Puedes hacer más con tus placas</h2>
+
+<div class="m-4 md:m-0 grid grid-cols-12 gap-4">
+    <div class="col-span-12 md:col-span-4 md:col-start-2">
+        <article class="overflow-hidden rounded-lg shadow transition hover:shadow-lg">
+            <img
+              alt="Office"
+              src="https://i.ibb.co/k9pXDR5/Post-facebook-venta-de-producto-comercial-moderno-gris-1.gif"
+              class="h-56 w-full object-cover"
+            />
+
+            <div class="bg-white p-4 sm:p-6">
+
+
+              <a href="#">
+                <h3 class="mt-0.5 text-lg text-gray-900">
+                  Recicla tu plaquita.
+                </h3>
+              </a>
+
+              <p class="mt-2 line-clamp-3 text-sm/relaxed text-gray-500">
+                Si tu placa se encuentra en óptimas condiciones, puedes considerar donarla a una fundación. Ellos se ponen contacto con nosotros para activar la placa en modo organización.              </p>
+            </div>
+          </article>
+    </div>
+    <div class="col-span-12 md:col-span-6">
+        <article class="overflow-hidden rounded-lg shadow transition hover:shadow-lg">
+            <img
+              alt="Office"
+              src="https://i.ibb.co/gtJSLdB/image.png"
+              class="h-56 w-full object-cover"
+            />
+
+            <div class="bg-white p-4 sm:p-6">
+
+
+              <a href="#">
+                <h3 class="mt-0.5 text-lg text-gray-900">
+                    Regala una placa de identificación a quien la necesita
+                </h3>
+              </a>
+
+              <p class="mt-2 line-clamp-3 text-sm/relaxed text-gray-500">
+                Puedes contribuir donando una placa especializada a los refugios de animales. Esta placa ha sido diseñada con características específicas que les permitirán gestionar de manera más eficaz la información de cada mascota       </p>
+              </p>
+            </div>
+          </article>
+
+
+    </div>
+</div>
+
+
