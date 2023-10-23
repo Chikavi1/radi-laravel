45a9b325c568c88fd9399f313e540e18484dcb59
chikavi1 <chikavi@hotmail.com>
2023-10-23 16:59:18 -0600

    radi img compress

--- resources/views/home/placas.blade.php
+++ resources/views/home/placas.blade.php
@@ -11,7 +11,7 @@ href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
     </h1>
 
     <div class="w-full mx-auto mt-2 text-center ">
-        <img src="https://i.ibb.co/K5QgHVj/Nombre-de-tu-mascota-4.png " alt="id radi" class="animate__animated animate__rollIn w-72 mx-auto" />
+        <img src="https://i.ibb.co/GMbmqX1/Nombre-de-tu-mascota-4-1.png" alt="id radi" class="animate__animated animate__rollIn w-72 mx-auto" />
       </div>
 
     <div class="text-center">
