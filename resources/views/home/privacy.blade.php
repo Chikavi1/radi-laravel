2d396f0ae22512ac0cfbc634a452bd166a50fc0a
chikavi1 <chikavi@hotmail.com>
2024-04-25 22:07:33 -0600

    privacy add

--- resources/views/home/privacy.blade.php
+++ resources/views/home/privacy.blade.php
@@ -2,8 +2,34 @@
 <div class='m-8 md:px-52'>
     <h2 class='font-bold text-3xl'>POLÍTICA DE PRIVACIDAD</h2>
     <p class='mt-2 text-xl'>El presente Política de Privacidad establece los términos en que Radi usa y protege la información que es proporcionada por sus usuarios al momento de utilizar su sitio web. Esta compañía está comprometida con la seguridad de los datos de sus usuarios. Cuando le pedimos llenar los campos de información personal con la cual usted pueda ser identificado, lo hacemos asegurando que sólo se empleará de acuerdo con los términos de este documento. Sin embargo esta Política de Privacidad puede cambiar con el tiempo o ser actualizada por lo que le recomendamos y enfatizamos revisar continuamente esta página para asegurarse que está de acuerdo con dichos cambios.</p>
-    <h2 class='font-bold text-3xl'>Información que es recogida</h2>
-    <p class='mt-2 text-xl'> Nuestro sitio web podrá recoger información personal por ejemplo: Nombre,  información de contacto como  su dirección de correo electrónica e información demográfica. Así mismo cuando sea necesario podrá ser requerida información específica para procesar algún pedido o realizar una entrega o facturación.</p>
+   <h2 class='font-bold text-3xl'>Información Recopilada</h2>
+    <p class="mt-2 text-xl">
+        La Aplicación recopila la siguiente información:
+        <br>
+        Datos de Ubicación: La aplicación puede recopilar datos de ubicación en tiempo real de su dispositivo móvil mientras la aplicación está en uso o en segundo plano. Estos datos se utilizan para proporcionar servicios de seguimiento de la ubicación de la mascota.
+        <br>
+        Información del Dispositivo: La aplicación puede recopilar información sobre el dispositivo móvil, incluyendo el modelo del dispositivo, el sistema operativo, el identificador único del dispositivo y otra información similar.
+
+    </p>
+    <h2 class='font-bold text-3xl'>Uso de la Información</h2>
+    <p class="mt-2 text-xl">
+        Utilizamos la información recopilada para:
+
+        Proporcionar y mantener los servicios de seguimiento de ubicación de paseo de mascotas. <br>
+        Mejorar y personalizar la experiencia del usuario. <br>
+        Comprender cómo los usuarios interactúan con la aplicación y realizar mejoras en función de esa información. <br>
+        Enviar notificaciones relacionadas con el servicio, como recordatorios de paseos programados y actualizaciones de ubicación de la mascota. <br>
+    </p>
+    <h2 class='font-bold text-3xl'>Compartir Informació</h2>
+
+<p class="mt-2 text-xl">
+    No compartimos información personal identificable con terceros, excepto en las siguientes circunstancias:
+    <br>
+    Con su consentimiento explícito. <br>
+    Cuando sea necesario para cumplir con las leyes aplicables, regulaciones gubernamentales o solicitudes legales. <br>
+    Para proteger la seguridad y los derechos de los usuarios, así como para proteger nuestros derechos y propiedad. <br>
+
+</p>
 
     <h2 class='font-bold text-3xl'>Uso de la información recogida</h2>
     <p class='mt-2 text-xl'>Nuestro sitio web emplea la información con el fin de proporcionar el mejor servicio posible, particularmente para mantener un registro de usuarios, de pedidos en caso que aplique, y mejorar nuestros productos y servicios.  Es posible que sean enviados correos electrónicos periódicamente a través de nuestro sitio con ofertas especiales, nuevos productos y otra información publicitaria que consideremos relevante para usted o que pueda brindarle algún beneficio, estos correos electrónicos serán enviados a la dirección que usted proporcione y podrán ser cancelados en cualquier momento.</p>
